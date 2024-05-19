15dc72739b8ca3633c2502799cf1887bb0180f02
yunkot <yunkot@gmail.com>
2024-05-19 13:35:22 -0600

    Integrated support for LPC11xx based embedded boards.

--- Source/PXL.Logs.pas
+++ Source/PXL.Logs.pas
@@ -11,6 +11,7 @@ unit PXL.Logs;
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  *)
+{< Helper functions for debugging purposes. }
 interface
 
 {$INCLUDE PXL.Config.inc}
